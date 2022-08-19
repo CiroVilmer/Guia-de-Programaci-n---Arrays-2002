48aa5d15e0f13eb98dd5ef734773940244119682
CiroVilmer <101563425+CiroVilmer@users.noreply.github.com>
2022-08-19 10:42:04 -0300

    ej 1 hecho y ej 2 empezado

---
+++ Assets/Scripts/Ej1_Puntajes.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class Ej1_Puntajes : MonoBehaviour
+{
+    [SerializeField] int[] puntajes = new int[10];
+
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("El mayor puntaje es " + ObtenerMayorPuntaje());
+        Debug.Log("El nivel con menor puntaje es " + ObtenerNivelDeMenorPuntaje());
+        Debug.Log("El promedio es " + ObtenerPuntajePromedio());
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    int ObtenerMayorPuntaje()
+    {
+        int mayorPuntaje = 0;
+
+        foreach(int puntaje in puntajes)
+        {
+            if(puntaje < mayorPuntaje)
+            {
+                mayorPuntaje = puntaje;
+            }
+
+        }
+
+        return mayorPuntaje;
+    }
+
+    int ObtenerNivelDeMenorPuntaje()
+    {
+        int indiceNivelMenorPuntaje = 0;
+        int menorPuntaje = puntajes[0];
+
+        for(int i = 0; i < puntajes.Length; i++)
+        {
+            
+            if (puntajes[i] < menorPuntaje)
+            {
+                menorPuntaje = puntajes[i];
+                indiceNivelMenorPuntaje = i;
+            }
+        }
+
+        return indiceNivelMenorPuntaje;
+    }
+
+    float ObtenerPuntajePromedio()
+    {
+        float sumaPuntajes = 0;
+        float promedio = 0;
+
+        foreach(float puntaje in puntajes)
+        {
+            sumaPuntajes += puntaje; 
+        }
+
+        promedio = sumaPuntajes / puntajes.Length;
+
+        return promedio;
+
+    }
+
+}
---
+++ Assets/Scripts/Ej2_Dias.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ej2_Dias : MonoBehaviour
+{
+    [SerializeField] string[] nombreMeses = new string[12];
+    [SerializeField] int[] diasPorCadaMes = new int[12];
+    public string fechaIngresada;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ValidarFormatoFecha(fechaIngresada);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    bool ValidarFormatoFecha(string fecha)
+    {
+        bool fechaValida = false;
+
+        if(fecha.Length == 4)
+        {
+            string dia = "";
+            string mes = "";
+            dia += fecha[0];
+            dia += fecha[1];
+            int diaNumber = int.Parse(dia);
+            Debug.Log(dia);
+
+            mes += fecha[2];
+            mes += fecha[3];
+            int mesNumber = int.Parse(mes);
+            Debug.Log(mes);
+
+            if(diaNumber > 0 && diaNumber <= 31 && mesNumber > 0 && mesNumber <= 12)
+            {
+                fechaValida = true;
+            }
+
+        }
+        else
+        {
+            Debug.Log("El largo de la fecha es incorrecto");
+        }
+
+        return fechaValida;
+    }
+}
